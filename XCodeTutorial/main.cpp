05da061d5d49da34613099136b5253dbd95d72a9
xplane <rizzo@Nicholass-MacBook-Pro-2.local>
2015-04-03 15:01:08 -0500

    Trying to commit for once.

--- XCodeTutorial/main.cpp
+++ XCodeTutorial/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  XCodeTutorial
-//
-//  Created by Nicholas Rizzo on 4/2/15.
-//  Copyright (c) 2015 Nicholas Rizzo. All rights reserved.
-//
-
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    
+    int choice;
+    double dollar = 0, peso = 0;
+    char Quit = ' ';
+    while (Quit != 'n'){
+        
+        cout << "Press 1 to convert Dollars to Pesos or press 2 to convert Pesos to Dollars: ";
+        cin >> choice;
+        
+        if (choice == 1) {
+            cout << "Enter how many dollars you want to convert: ";
+            cin >> dollar;
+            peso = dollar * 14.87;
+            cout << dollar << " dollars converts to " << peso << endl;
+        }
+        if (choice == 2) {
+            cout << "\nEnter how many Pesos you want to convert: ";
+            cin >> peso;
+            dollar = peso / 14.87;
+            cout << "Pesos converts to: " << dollar;
+        }
+        
+        
+        cout << "\nDo you want to run again? Y/N? ";
+        cin >> Quit;
+    }
+    
     return 0;
 }
